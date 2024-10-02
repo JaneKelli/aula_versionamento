004422cbb096f07ea7873a7ac892ff83950f995d
JaneKelli <janepessoa@alunos.utfpr.edu.br>
2024-10-02 20:03:10 -0300

    array de frutas

---
+++ frutas.js
@@ -0,0 +1,5 @@
+const frutas = ["goiaba", "abacaxi", "melão", "banana", "manga", "mamão", "limão", "morango", "pitanga", "laranja", "maçã"];
+
+console.log("fruta na posição 0:", frutas[0]);
+console.log("fruta na posição 3:", frutas[3]);
+console.log("array tem", frutas.length, "frutas");
