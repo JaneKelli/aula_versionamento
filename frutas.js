8c4b9ed31e228d2b6ae23117e742f88d5b807594
janepessoa@alunos.utfpr.edu.br <janepessoa@alunos.utfpr.edu.br>
2024-10-02 21:03:53 -0300

    array frutas

--- frutas.js
+++ frutas.js
@@ -1,7 +1,9 @@
-const frutas = ["goiaba", "abacaxi", "melão", "banana", "manga", "mamão", "limão", "morango", "pitanga", "laranja", "maçã"];
+const frutas = 
+["goiaba", "abacaxi", "melão", "banana", "manga", "mamão", "limão", "morango", "pitanga", "laranja", "maçã", "uva", "melancia", "acerola", "mexerica"];
 
 console.log("fruta na posição 0:", frutas[0]);
 console.log("fruta na posição 3:", frutas[3]);
 console.log("fruta na posição 7:", frutas[7]);
 console.log("fruta na posição 10:", frutas[10]);
 console.log("array tem", frutas.length, "frutas");
+console.log("fruta na posição 12:", frutas[14]);
