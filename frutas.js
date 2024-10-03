159fad9de4e29377675006c480117eb80c2350ae
janepessoa@alunos.utfpr.edu.br <janepessoa@alunos.utfpr.edu.br>
2024-10-02 21:43:53 -0300

    array de frutas

--- frutas.js
+++ frutas.js
@@ -6,4 +6,4 @@ console.log("fruta na posição 3:", frutas[3]);
 console.log("fruta na posição 7:", frutas[7]);
 console.log("fruta na posição 10:", frutas[10]);
 console.log("array tem", frutas.length, "frutas");
-console.log("fruta na posição 12:", frutas[14]);
+console.log("fruta na posição 12:", frutas[frutas.length - 1]);
